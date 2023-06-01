f61b1699610d23e59b198f0c928fb4dbe288f3b1
manzanile@gmail.com <Qqerrordeconexion>
2023-06-01 19:51:36 +0200

    feat: Added find methods for SizesRepository and StockRepository

--- src/main/java/com/assessment/mca/productsviewer/model/repository/SizesRepository.java
+++ src/main/java/com/assessment/mca/productsviewer/model/repository/SizesRepository.java
@@ -2,8 +2,14 @@ package com.assessment.mca.productsviewer.model.repository;
 
 import com.assessment.mca.productsviewer.model.entities.Sizes;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SizesRepository extends JpaRepository<Sizes, String> {
+    @Query("select s from Sizes s join s.product p where p.id = :productId")
+    List<Sizes> findSizesByProductId(@Param("productId") String productId);
 }
--- src/main/java/com/assessment/mca/productsviewer/model/repository/StockRepository.java
+++ src/main/java/com/assessment/mca/productsviewer/model/repository/StockRepository.java
@@ -2,8 +2,14 @@ package com.assessment.mca.productsviewer.model.repository;
 
 import com.assessment.mca.productsviewer.model.entities.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface StockRepository extends JpaRepository<Stock, String> {
+    @Query("select st from Stock st join st.size si where si.id = :sizeId")
+    List<Stock> findStocksBySizesId(@Param("sizeId") String sizeId);
 }
